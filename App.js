07da3e5a8c5c5b6e78fd75399f4dbf0e1edf8a6b
nllew <n.lewis.perc@gmail.com>
2021-05-16 20:03:58 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { StyleSheet, ScrollView, TextInput, View } from "react-native";
+import Heading from "./src/components/Heading";
+import Input from "./src/components/Input";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: "",
+            todos: [],
+            type: "All",
+        };
+    }
+    inputChange(inputValue) {
+        console.log(" Input Value: ", inputValue);
+        this.setState({ inputValue });
+    }
+    render() {
+        const { inputValue } = this.state;
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                    keyboardShouldPersistTaps="always"
+                    style={styles.content}
+                >
+                    <Heading />
+                    <Input
+                        inputValue={inputValue}
+                        inputChange={(text) => this.inputChange(text)}
+                    />
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60,
+    },
 });
+
+export default App;
